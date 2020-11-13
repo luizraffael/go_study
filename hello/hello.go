a8b0b9d3de3e0d47df6b364814505bf8b049951b
luizraffael <luizrafael.sm@gmail.com>
2020-11-13 00:14:39 -0300

    Commit - Hello com funcoes de erro
    Commit - Hello com funcoes de erro

---
+++ hello/hello.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	//Set properties of the predefined logger, including
+	//the log entry prefix and a flag to disable printing
+	//the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	//Request a greeting message.
+	message, err := greetings.Hello("Maria")
+	//if an error was returned, print it to the console and
+	//exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	//if no error was returned, print the returned message
+	//to the console
+	//É nois que fly
+	fmt.Println(message)
+}

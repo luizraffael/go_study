8d4302a6b058d46e92e9e02576a252767fdf4a5b
Luiz Rafael <luizrafael.sm@gmail.com>
2021-01-20 12:19:43 -0300

    Commit refactored

--- go_tdd/hello/hello.go
+++ go_tdd/hello/hello.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+const prefixHello = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == ""{
+		name = "world"
+	}
+	return prefixHello + name
 }
 
 func main(){
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello(""))
 }
--- go_tdd/hello/hello_test.go
+++ go_tdd/hello/hello_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-	result := Hello("Chris")
-	expected := "Hello, Chris"
 
-	if result != expected {
-		t.Errorf("Result '%s', expected '%s'", result, expected)
+	verifyCorrectMessage := func(t *testing.T, result, expected string){
+		t.Helper()
+		if result != expected{
+			t.Errorf("result '%s', expected '%s'", result, expected)
+		}	
 	}
+
+	t.Run("Say hello to people", func(t *testing.T){
+		result := Hello("Chris")
+		expected := "Hello, Chris"
+		verifyCorrectMessage(t, result, expected)
+	
+	})
+
+	t.Run("'Hello, world' when 'string' is empty", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, world"
+
+		verifyCorrectMessage(t, result, expected)
+
+	})
+
+
+	
 }

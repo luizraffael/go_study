c16d01b43a12057f2d46aa36dae4ce67df130803
Luiz Rafael <luizrafael.sm@gmail.com>
2021-01-22 00:30:05 -0300

    update sum, refactored

--- go_tdd/array_slice/sum.go
+++ go_tdd/array_slice/sum.go
@@ -2,8 +2,8 @@ package array_slice
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, numbers := range numbers {
+		sum += numbers
 	}
 	return sum
 }

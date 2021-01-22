21f080422695fc08a276451c7c28ac31e3baa1b1
Luiz Rafael <luizrafael.sm@gmail.com>
2021-01-22 00:42:01 -0300

    Commit slice (dinamic array)

--- go_tdd/array_slice/sum_test.go
+++ go_tdd/array_slice/sum_test.go
@@ -3,12 +3,25 @@ package array_slice
 import "testing"
 
 func TestSoma(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Colection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	result := Sum(numbers)
-	expected := 15
+		result := Sum(numbers)
+		expected := 15
 
-	if expected != result {
-		t.Errorf("Result %d, expected %d, data %v", result, expected, numbers)
-	}
+		if expected != result {
+			t.Errorf("Result %d, expected %d, data %v", result, expected, numbers)
+		}
+	})
+
+	t.Run("Dinamic collection", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		result := Sum(numbers)
+		expected := 6
+
+		if expected != result {
+			t.Errorf("Result %d, expected %d, data %v", result, expected, numbers)
+		}
+	})
 }
